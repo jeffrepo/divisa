535c326713c964097071efbccbe6dadd25ca4e2d
jeffrepo <jeffer.leo93@gmail.com>
2021-08-30 09:46:35 -0600

    update

--- __manifest__.py
+++ __manifest__.py
@@ -19,8 +19,9 @@
     'data': [
         'data/ir_sequence_data.xml',
         'views/divisa_views.xml',
-        # 'views/account_payment_view.xml',
-        # 'views/recibo_pago_views.xml',
         'security/ir.model.access.csv',
+        'views/reporte_divisa.xml',
+        'views/report.xml',
+
     ],
 }
